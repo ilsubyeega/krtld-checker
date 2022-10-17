897c2b12b0823bdcf7fd4f0e5b890510bfc00318
rotoblur <git@roto.win>
2022-10-17 17:57:05 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use dotenv::dotenv;
+use miette::{bail, IntoDiagnostic};
 use reqwest::Url;
 use serde_json::Value;
 use std::{env, str::FromStr};
@@ -38,18 +39,14 @@ async fn main() {
 }
 
 fn generate_arr() -> Vec<String> {
-    let alphabets = "abcdefghijklnmopqrstuvwxyz".as_bytes();
+    let alphabets = 'a'..='z';
 
     let mut string_arr: Vec<String> = vec![];
 
     for a in alphabets {
         for b in alphabets {
             for c in alphabets {
-                let mut str = String::new();
-                str.push(a.clone() as char);
-                str.push(b.clone() as char);
-                str.push(c.clone() as char);
-                string_arr.push(str);
+                string_arr.push(format!("{a}{b}{c}"));
             }
         }
     }
@@ -57,10 +54,7 @@ fn generate_arr() -> Vec<String> {
     string_arr
 }
 
-async fn check_domain_available(
-    api_key: &str,
-    domain: &str,
-) -> Result<bool, Box<dyn std::error::Error>> {
+async fn check_domain_available(api_key: &str, domain: &str) -> miette::Result<bool> {
     println!("Checking {}.kr", domain);
 
     let url = Url::parse_with_params(
@@ -69,37 +63,39 @@ async fn check_domain_available(
             ("serviceKey", api_key),
             ("query", &format!("{}.kr", domain)),
         ],
-    )?;
+    )
+    .into_diagnostic()?;
 
-    let resp = reqwest::get(url).await?.text().await?;
+    let resp = reqwest::get(url)
+        .await
+        .into_diagnostic()?
+        .text()
+        .await
+        .into_diagnostic()?;
     let from_str = serde_json::from_str(&resp);
 
-    if from_str.is_err() {
-        println!("{}", &resp);
-        Err("Couldn't parse data.")?
-    }
-    let parsed: Value = from_str.unwrap();
+    let parsed: Value = from_str.or_else(|_| {
+        eprintln!("{}", &resp);
+        bail!("Could not parse data.")
+    })?;
 
     let result_code = &parsed["response"]["result"]["result_code"];
     if *result_code == Value::Null {
-        println!("The data seems to be wrong: {}", resp);
-        Err("Wrong data")?
+        eprintln!("The data seems to be wrong: {}", resp);
+        bail!("Wrong Data")
     } else {
         let result_code = result_code.as_str().unwrap_or("000");
-        let result_code: i32 = FromStr::from_str(result_code)?;
+        let result_code = i32::from_str(result_code).into_diagnostic()?;
 
         match result_code {
-            0 => Err("result_code was null")?,
-            22 => Err("The api key seems to be wrong")?,
+            0 => bail!("result_code was null"),
+            22 => bail!("The api key seems to be wrong"),
             100 => Ok(false),
             113 => Ok(false), // 상기 도메인이름은 도메인이름의 안정적 관리와 공공의 이익 등을 위하여등록이 제한된 도메인이름입니다
             10000 => Ok(true),
             _ => {
                 println!("{}", &resp);
-                Err(format!(
-                    "dunno about this type of result code: {}",
-                    result_code
-                ))?
+                bail!("dunno about this type of result code: {}", result_code)
             }
         }
     }
